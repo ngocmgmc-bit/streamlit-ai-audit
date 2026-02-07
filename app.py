897847da1be54552f4deed58f612f27f2a76dde4
ngocmgmc-bit <ngoc.mgmc@gmail.com>
2026-02-07 14:43:13 +0700

    add initial app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="AI Audit", layout="wide")
+
+st.title("AI AUDIT (ver 1.0)")
+st.caption("Phiên bản thử nghiệm")
+
+st.write("Ứng dụng đã deploy thành công 🚀")
